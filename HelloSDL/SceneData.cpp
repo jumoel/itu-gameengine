d8b5ae93e3b021d3146203d78bf23525df3df7dc
Julian Møller <julian.moeller@gmail.com>
2011-09-20 11:56:46 +0200

    Add scene graph parsing

--- HelloSDL/SceneData.cpp
+++ HelloSDL/SceneData.cpp
@@ -4,15 +4,26 @@ Object* createGraph()
 {
 	auto root = new Object();
 
-	auto triangle1 = new Object();
+	auto m1 = new Matrix4x4f();
+	m1->Translate(0, 0, -4);
+	root->transformation = m1;
 
-	triangle1->vertices->push_back(*(new Vector3f()));
-	triangle1->vertices->push_back(*(new Vector3f(1, 1, 0)));
-	triangle1->vertices->push_back(*(new Vector3f(1, 0, 0)));
+	auto triangle1 = new Object();
+	auto v1 = new Vector3f(0, 0, 0);
+	auto v2 = new Vector3f(1, 1, 0);
+	auto v3 = new Vector3f(1, 0, 0);
 	
-	triangle1->colors->push_back(*(new Vector3f(1, 0, 0)));
-	triangle1->colors->push_back(*(new Vector3f(0, 1, 0)));
-	triangle1->colors->push_back(*(new Vector3f(0, 0, 1)));
+	auto c1 = new Vector3f(1, 0, 0);
+	auto c2 = new Vector3f(0, 1, 0);
+	auto c3 = new Vector3f(0, 0, 1);
+
+	triangle1->vertices->push_back(*v1);
+	triangle1->vertices->push_back(*v2);
+	triangle1->vertices->push_back(*v3);
+	
+	triangle1->colors->push_back(*c1);
+	triangle1->colors->push_back(*c2);
+	triangle1->colors->push_back(*c3);
 
 	root->children->push_back(*triangle1);
 
