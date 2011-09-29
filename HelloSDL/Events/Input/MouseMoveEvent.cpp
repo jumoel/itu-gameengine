a9ba78cb9c419431ce24bd0d15f73179f6fad152
Emil Erik Hansen <emil@gnueless.com>
2011-09-29 11:14:50 +0200

    TestEvent now catches MouseMovement.

--- HelloSDL/Events/Input/MouseMoveEvent.cpp
+++ HelloSDL/Events/Input/MouseMoveEvent.cpp
@@ -1,9 +1,9 @@
 #include "MouseMoveEvent.h"
-#include "SDL_events.h"
+
 
 MouseMoveEvent::MouseMoveEvent(SDL_MouseMotionEvent *input, short eventtype) : EventObject(eventtype)
 {
-
+	this->input = input;
 }
 
 
