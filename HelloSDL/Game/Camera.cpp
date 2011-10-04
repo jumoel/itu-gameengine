b9566f63221cee7a1a5ee13163ba6377d8529dc3
pyjamads <pyjamads@gmail.com>
2011-10-04 09:27:09 +0200

    Changed mouse looking

--- HelloSDL/Game/Camera.cpp
+++ HelloSDL/Game/Camera.cpp
@@ -1,5 +1,9 @@
 #include "Camera.h"
 #include "..\Managers\InputManager.h"
+#include "..\Math\Matrix4x4f.h"
+#include <cmath>
+
+/* Region Construction & Deconstruction */
 
 Camera::Camera()
 {
@@ -9,17 +13,213 @@ Camera::Camera()
 	InputManager::RegisterKeyboardEventHandler(this);
 	InputManager::RegisterMouseClickEventHandler(this);
 	InputManager::RegisterMouseMoveEventHandler(this);
+
+	// TODO Enforce these Rules:
+	/* |Position - LookAt| = 1 */
+	/* |Up| = 1 */
+	/* Look vector dotProduct Up = 0  */
 }
 
 Camera::~Camera()
 {
 }
 
+/* Endregion Construction & Deconstruction */
+
+//------------------------------------------------------------------------------------------------------------//
+
+/* Region Camera Controls */
+
+// Moves the position of the camera to the new location.
+/* Implemented as moving the camera, up/down in a 2D plane, othogonally to the z axis, of the LookAt vector */
+void Camera::MoveCamera3D(Vector3f *newPosition)
+{
+	float xDirection = newPosition->x() - Position.x();
+	float yDirection = newPosition->y() - Position.y();
+	float zDirection = newPosition->z() - Position.z();
+
+	Position.SetX(newPosition->x());
+	Position.SetY(newPosition->y());
+	Position.SetZ(newPosition->z());
+	
+	LookAt.SetX(xDirection + LookAt.x());
+	LookAt.SetY(yDirection + LookAt.y());
+	LookAt.SetZ(zDirection + LookAt.z());
+}
+
+//Rotates the camera around the x axis of the LookAt vector. Positive is clockwise, negative is counter-clockwise.
+//Limited to 90° Up/Down
+void Camera::PitchCamera(float radians)
+{
+	float xUp = Up.x();
+	float yUp = Up.y();
+	float zUp = Up.z();
+
+	float rUpPlusTx = xUp;
+	float rUpPlusTy = yUp*cos(radians) - zUp*sin(radians);
+	float rUpPlusTz = yUp*sin(radians) + zUp*cos(radians);
+
+	if(rUpPlusTy <= 0)
+	{
+		return;
+	}
+
+	Up.SetY(rUpPlusTy);
+	Up.SetZ(rUpPlusTz);
+
+	float xPosition = Position.x();
+	float yPosition = Position.y();
+	float zPosition = Position.z();
+
+	float xLookDirection = LookAt.x() - Position.x();
+	float yLookDirection = LookAt.y() - Position.y();
+	float zLookDirection = LookAt.z() - Position.z();
+
+	float rLookPlusTx =	xLookDirection + xPosition;
+	float rLookPlusTy = yLookDirection*cos(radians) - zLookDirection*sin(radians) + yPosition;
+	float rLookPlusTz = yLookDirection*sin(radians) + zLookDirection*cos(radians) + zPosition;
+
+	LookAt.SetX(rLookPlusTx);
+	LookAt.SetY(rLookPlusTy);
+	LookAt.SetZ(rLookPlusTz);
+}
+
+//Rotates the camera around the y axis, of Up vector. Positive is clockwise, negative is counter-clockwise.
+void Camera::YawCamera(float radians)
+{
+	float xPosition = Position.x();
+	float yPosition = Position.y();
+	float zPosition = Position.z();
+
+	float xLookDirection = LookAt.x() - Position.x();
+	float yLookDirection = LookAt.y() - Position.y();
+	float zLookDirection = LookAt.z() - Position.z();
+	
+	float rLookPlusTx = xLookDirection*cos(radians) - zLookDirection*sin(radians) + xPosition;
+	float rLookPlusTy = yLookDirection + yPosition;
+	float rLookPlusTz = xLookDirection*sin(radians) + zLookDirection*cos(radians) + zPosition;
+
+	LookAt.SetX(rLookPlusTx);
+	LookAt.SetY(rLookPlusTy);
+	LookAt.SetZ(rLookPlusTz);
+}
+
+//Rotates the camera around the z axis, of the LookAt vector. Positive is clockwise, negative is counter-clockwise.
+void Camera::RollCamera(float radians)
+{
+	float xLook = LookAt.x();
+	float yLook = LookAt.y();
+	float zLook = LookAt.z();
+	float xUp = Up.x();
+	float yUp = Up.y();
+	float zUp = Up.z();
+	
+
+}
+
+//Zooms the camera in or out, Negative is out and Positive is in.
+/* Implemented as moving the camera in/out in a line parallel to the z axis, of the LookAt vector */
+void Camera::ZoomCamera(float distance)
+{
+	float xDirection = LookAt.x() - Position.x();
+	float yDirection = LookAt.y() - Position.y();
+	float zDirection = LookAt.z() - Position.z();
+
+	xDirection *= distance;
+	yDirection *= distance;
+	zDirection *= distance;
+
+	Position.SetX(xDirection + Position.x());
+	Position.SetY(yDirection + Position.y());
+	Position.SetZ(zDirection + Position.z());
+
+	LookAt.SetX(xDirection + LookAt.x());
+	LookAt.SetY(yDirection + LookAt.y());
+	LookAt.SetZ(zDirection + LookAt.z());
+}
+
+// Moves the camera in a direction.
+/* Implemented as moving the camera, up/down in a 2D plane, othogonally to the z axis, of the LookAt vector */
+void Camera::MoveCameraUpDown2D(float distance)
+{
+	float xDirection = Up.x();
+	float yDirection = Up.y();
+	float zDirection = Up.z();
+
+	xDirection *= distance;
+	yDirection *= distance;
+	zDirection *= distance;
+
+	Position.SetX(xDirection + Position.x());
+	Position.SetY(yDirection + Position.y());
+	Position.SetZ(zDirection + Position.z());
+
+	LookAt.SetX(xDirection + LookAt.x());
+	LookAt.SetY(yDirection + LookAt.y());
+	LookAt.SetZ(zDirection + LookAt.z());
+}
+
+void Camera::MoveCameraLeftRight2D(float distance)
+{
+	float dotProduct = 0;
+
+	float xLook = LookAt.x() - Position.x();
+	float yLook = LookAt.y() - Position.y();
+	float zLook = LookAt.z() - Position.z();
+
+	dotProduct = Up.x()*xLook + Up.y()*yLook + Up.z()*zLook;
+	
+	if(dotProduct != 0.0f)
+	{
+		return;
+	}
+
+	//Create the Cross Product vector, Perpendicular to the Up/LookAt plane
+	float xDirection = Up.y() * zLook - Up.z() * yLook;
+	float yDirection = Up.z() * xLook - Up.x() * zLook;
+	float zDirection = Up.x() * yLook - Up.y() * xLook;
+
+	xDirection *= distance;
+	yDirection *= distance;
+	zDirection *= distance;
+
+	Position.SetX(xDirection + Position.x());
+	Position.SetY(yDirection + Position.y());
+	Position.SetZ(zDirection + Position.z());
+
+	LookAt.SetX(xDirection + LookAt.x());
+	LookAt.SetY(yDirection + LookAt.y());
+	LookAt.SetZ(zDirection + LookAt.z());
+}
+
+void Camera::ResetCamera()
+{
+	Position.SetX(0);
+	Position.SetY(0);
+	Position.SetZ(0);
+
+	LookAt.SetX(0);
+	LookAt.SetY(0);
+	LookAt.SetZ(-1);
+
+	Up.SetX(0);
+	Up.SetY(1);
+	Up.SetZ(0);
+}
+
+/* Endregion Camera Controls */
+
+//------------------------------------------------------------------------------------------------------------//
+
+/* Region Event handling */
+
+//**************************************************************************************************//
+
+/* Region Mouse Event handling */
+
 void Camera::OnButtonDown(MouseClickEvent *button)
 {
 	auto buttonInput = button->GetInput();
-
-	float posz = 0;
 
 	switch(buttonInput->button)
 	{
@@ -27,18 +227,16 @@ void Camera::OnButtonDown(MouseClickEvent *button)
 		case 1:
 			isLeftButtonDown = true;
 			break;
-		case 2:
+		case 3:
 			isRightButtonDown = true;
 			break;
 
 		//Zoom control on scrollwheel
 		case 4:
-			posz = this->Position.z();
-			this->Position.SetZ(posz - 0.5f);
+			ZoomCamera(0.5f);
 			break;
 		case 5:
-			posz = this->Position.z();
-			this->Position.SetZ(posz + 0.5f);
+			ZoomCamera(-0.5f);
 			break;
 	}
 }
@@ -53,7 +251,7 @@ void Camera::OnButtonUp(MouseClickEvent *button)
 		case 1:
 			isLeftButtonDown = false;
 			break;
-		case 2:
+		case 3:
 			isRightButtonDown = false;
 			break;
 	}
@@ -73,69 +271,73 @@ void Camera::OnMotion(MouseMoveEvent *motion)
 
 	if(isLeftButtonDown)
 	{
-		lookx = this->LookAt.x();
-		this->LookAt.SetX(lookx + (motionInput->xrel * 0.01f));
-
-		looky = this->LookAt.y();
-		this->LookAt.SetY(looky + (motionInput->yrel * 0.01f));
+		PitchCamera(motionInput->yrel * 0.01f);
+		YawCamera(-motionInput->xrel * 0.01f);
 	}
 	else if(isRightButtonDown)
 	{
+		// TESTING FACILITIES !
+		// REMOVE OR REDO WHEN TESTING IS DONE!
+		lookx = this->LookAt.x();
+		lookz = this->LookAt.z();
+		
+		auto asinLookx = asin(lookx);
+		auto asinLookz = asin(lookz);
 
+		this->LookAt.SetX( sinf( asinLookx + (motionInput->xrel * 0.1f)));
+		this->LookAt.SetZ( sinf( asinLookz + (motionInput->xrel * 0.1f)));
+
+		/*looky = this->LookAt.y();
+		this->LookAt.SetY(looky + (motionInput->yrel * 0.01f));
+		*/
+		
+		
+
+		/*upz = this->Up.z();
+		this->Up.SetZ(upz + (motionInput->yrel * 0.01f));*/
 	}
 }
+
+/* Endregion Mouse Event handling */
+
+//**************************************************************************************************//
+
+/* Region Keyboard Event handling */
 
 void Camera::OnKeyDown(KeyPressedEvent *key)
 {
 	auto keyInput = key->GetInput();
-	
-	float posx = 0;
-	float posy = 0;
-
-	float lookx = 0;
-	float looky = 0;
-
-	float upx = 0;
 
 	switch(keyInput->keysym.sym)
 	{
-		//Control position x
-		case SDLK_d:
-			posx = this->Position.x();
-			lookx = this->LookAt.x();
-			this->Position.SetX(posx + 0.5f);
-			this->LookAt.SetX(lookx + 0.5f);
-			break;
-		case SDLK_a:
-			posx = this->Position.x();
-			lookx = this->LookAt.x();
-			this->Position.SetX(posx - 0.5f);
-			this->LookAt.SetX(lookx - 0.5f);
-			break;
-
-		//Control position y
+		//Control position Up/Down
 		case SDLK_w:
-			posy = this->Position.y();
-			looky = this->LookAt.y();
-			this->Position.SetY(posy + 0.5f);
-			this->LookAt.SetY(looky + 0.5f);
+			MoveCameraUpDown2D(0.5f);
 			break;
 		case SDLK_s:
-			posy = this->Position.y();
-			looky = this->LookAt.y();
-			this->Position.SetY(posy - 0.5f);
-			this->LookAt.SetY(looky - 0.5f);
+			MoveCameraUpDown2D(-0.5f);
+			break;
+
+		//Control position Left/Right
+		case SDLK_d:
+			MoveCameraLeftRight2D(0.5f);
+			break;
+		case SDLK_a:
+			MoveCameraLeftRight2D(-0.5f);
 			break;
 
 		//Control rotation
 		case SDLK_q:
-			upx = this->Up.x();
-			this->Up.SetX(upx - 0.5f);
+			RollCamera(0.5f);
 			break;
 		case SDLK_e:
-			upx = this->Up.x();
-			this->Up.SetX(upx + 0.5f);
-			break;		
+			RollCamera(-0.5f);
+			break;	
+
+		//Reset the camera
+		case SDLK_SPACE:
+			ResetCamera();
+			break;
 	}
 }
 
@@ -143,3 +345,11 @@ void Camera::OnKeyUp(KeyPressedEvent *key)
 {
 
 }
+
+/* Endregion Keyboard Event handling */
+
+//**************************************************************************************************//
+
+/* Endregion Event handling */
+
+//------------------------------------------------------------------------------------------------------------//
