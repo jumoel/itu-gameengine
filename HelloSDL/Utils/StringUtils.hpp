5c4aa3b565bc1589dc595855db806f26cad1d600
Julian Møller <julian.moeller@gmail.com>
2011-11-01 13:39:09 +0100

    Change hash from lookup3 to superfasthash

---
+++ HelloSDL/Utils/StringUtils.hpp
@@ -0,0 +1,13 @@
+#ifndef ITUENGINE_STRINGUTILS_H
+#define ITUENGINE_STRINGUTILS_H
+
+class StringUtils
+{
+public:
+	static void Hash()
+	{
+		//hashlittle2()
+	}
+};
+
+#endif // ITUENGINE_STRINGUTILS_H
