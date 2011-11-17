ec494871a539ad83a7607611811d61ad4d3c1556
Emil Erik Hansen <emil@gnueless.com>
2011-11-17 12:12:10 +0100

    Basic MemoryManager layout added.

---
+++ ITUEngine/Managers/MemoryManager.cpp
@@ -0,0 +1,32 @@
+#include <Managers/MemoryManager.hpp>
+#include <iostream>
+#include <fstream>
+
+/* 
+ * Basic functions
+ */
+void MemoryManager::StartUp()
+{
+	// Here, a finite amount of memory needs to be allocated.
+}
+
+void MemoryManager::ShutDown()
+{
+	// All of the memory should be freed, everything destroyed... and such.
+}
+
+/* 
+ * MemoryManager specific functions
+ */
+
+void MemoryManager::Allocate()
+{
+	// Needs fitting input-value.
+	// Allocates memory from the MemoryManager-stack.
+}
+
+void MemoryManager::Free()
+{
+	// Needs fitting input-value.
+	// Frees memory from the MemoryManager-stack.
+}
