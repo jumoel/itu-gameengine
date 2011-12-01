b5cda6a1271ef3ed71684662e16eef6758af62ff
Steen Pedersen <steen.nordsmark@gmail.com>
2011-12-01 13:41:24 +0100

    created GetOGLPos

--- ITUEngine/Subsystems/Graphics/GraphicsSystem.cpp
+++ ITUEngine/Subsystems/Graphics/GraphicsSystem.cpp
@@ -8,9 +8,9 @@
 #include <Utils/ShaderUtils.hpp>
 #include "GL/glew.h"
 #include "GL/wglew.h"
-//#include <Globals.hpp>
-#include <Managers/SceneGraphManager.hpp>
 #include <Game/Camera.hpp>
+
+
 
 
 void GraphicsSystem::StartUp()
@@ -271,28 +271,7 @@ void GraphicsSystem::RenderRecursive(Object *obj)
 	glPopMatrix();
 }
 
-Vector3f GetOGLPos(float mousex, float mousey)
-	
-{
-    GLint viewport[4];
-    GLdouble modelview[16];
-    GLdouble projection[16];
-    GLint winX, winY, winZ;
-    GLdouble posX, posY, posZ;
- 
-    glGetDoublev( GL_MODELVIEW_MATRIX, modelview );
-    glGetDoublev( GL_PROJECTION_MATRIX, projection );
-    glGetIntegerv( GL_VIEWPORT, viewport );
- 
-	winX = mousex;
-	winY = mousey;
-    glReadPixels( int(winX), int(winY), 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &winZ );
- 
-    gluUnProject( winX, winY, winZ, modelview, projection, viewport, &posX, &posY, &posZ);
-	//std::cout<<"winX:"<<winX<<" , winY:"<<winY<<" , winZ:"<<winZ<<" , posX:"<<posX<<" , posY:"<<posY<<" , posZ:"<<posZ<<std::endl;
 
-    return Vector3f(posX, posY, posZ);
-}
 
 void GraphicsSystem::Render()
 {
@@ -397,7 +376,6 @@ void GraphicsSystem::Render()
 	//glMatrixMode(GL_MODELVIEW);
 
 
-	Vector3f collide = GetOGLPos(1.0f, 1.0f);
 	SDL_GL_SwapBuffers();
 }
 
