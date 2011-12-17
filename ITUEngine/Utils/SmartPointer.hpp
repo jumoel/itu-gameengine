75ee6ba4bb6c24679ef53c71521d5acd44a43441
Julian Møller <julian@julianmoeller.dk>
2011-12-17 12:05:23 +0100

    Add reference counter to smart pointer.

--- ITUEngine/Utils/SmartPointer.hpp
+++ ITUEngine/Utils/SmartPointer.hpp
@@ -1,17 +1,24 @@
 #ifndef ITUENGINE_SMARTPOINTER_HPP
 #define ITUENGINE_SMARTPOINTER_HPP
 
+#include <Utils/ReferenceCounter.hpp>
+
 template <typename T> class SmartPointer
 {
 private:
-	T* _data;
+	T *_data;
+	ReferenceCounter *_references;
 
 public:
+	SmartPointer(const SmartPointer<T>& sp);
 	SmartPointer(T* pointer);
+	SmartPointer();
+
 	~SmartPointer();
 
 	T& operator *();
 	T* operator ->();
+	SmartPointer<T>& operator =(const SmartPointer<T>& sp);
 
 };
 
