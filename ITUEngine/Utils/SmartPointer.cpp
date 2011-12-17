3076e1b3ffb1ab019151d0c3303c2946228703c9
Julian Møller <julian@julianmoeller.dk>
2011-12-17 11:49:27 +0100

    Fix syntax errors

--- ITUEngine/Utils/SmartPointer.cpp
+++ ITUEngine/Utils/SmartPointer.cpp
@@ -1,27 +1,25 @@
 #include <Utils/SmartPointer.hpp>
 
-/*
 template <typename T>
-SmartPointer::SmartPointer(T* pointer)
+SmartPointer<T>::SmartPointer(T* pointer)
 {
 	_data = pointer;
 }
 
 template <typename T>
-SmartPointer::~SmartPointer()
+SmartPointer<T>::~SmartPointer()
 {
 	delete _data;
 }
 
 template <typename T>
-T& SmartPointer::operator *()
+T& SmartPointer<T>::operator *()
 {
 	return *_data;
 }
 
 template <typename T>
-T* SmartPointer::operator ->()
+T* SmartPointer<T>::operator ->()
 {
 	return _data;
 }
-*/
